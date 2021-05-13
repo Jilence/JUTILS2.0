fb5033389ede23a164f8c7601b7fa8103e7f4bf2
l4zs <l4zs.contact@gmail.com>
2021-05-14 00:05:44 +0200

    use paper methods and refactoring

--- src/main/java/de/jilence/jutils/listener/ChatListener.java
+++ src/main/java/de/jilence/jutils/listener/ChatListener.java
@@ -1,23 +1,22 @@
 package de.jilence.jutils.listener;
 
+import io.papermc.paper.event.player.AsyncChatEvent;
+import net.kyori.adventure.text.Component;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 public class ChatListener implements Listener {
 
     @EventHandler(ignoreCancelled = true)
-    public void onAsyncChat(AsyncPlayerChatEvent event) {
+    public void onAsyncChat(AsyncChatEvent event) {
         Player player = event.getPlayer();
 
         if (player.isOp()) {
-            event.setFormat("§4Operator §8§l| §r§4%1$s§8: §r§7%2$s");
-            event.setMessage(ChatColor.translateAlternateColorCodes('&', event.getMessage()));
+            event.message(Component.text("§4Operator §8§l| §r§4" + player.getName() + ": §r§7" + ChatColor.translateAlternateColorCodes('&', event.message().toString())));
         } else {
-            event.setFormat("§7Spieler §8§l| §r§7%1$s§8: §r§7%2$s");
-
+            event.message(Component.text("§7Spieler §8§l| §r§7" + player.getName() + ": §r§7" + event.message()));
         }
     }
 }
