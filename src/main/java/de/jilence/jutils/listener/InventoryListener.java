63742cb857c246aa2fd109531235d2a40a823a54
l4zs <l4zs.contact@gmail.com>
2021-05-13 23:54:38 +0200

    use paper methods and refactoring

--- src/main/java/de/jilence/jutils/listener/InventoryListener.java
+++ src/main/java/de/jilence/jutils/listener/InventoryListener.java
@@ -23,20 +23,19 @@ public class InventoryListener implements Listener {
 
         Player player = (Player) event.getWhoClicked();
 
-        switch (event.getView().getTitle()) {
+        if (event.getCurrentItem() == null) return;
+        switch (event.getView().title().toString()) {
 
             case "§9Herausforderungen §8● ": {
                 event.setCancelled(true);
                 challengeInventoryClickEvent(event.getCurrentItem().getType(), (Player) event.getWhoClicked(), event.getClick(), event.getSlot());
                 break;
             }
-
             case "§9Spielregeln §8● ": {
                 event.setCancelled(true);
                 GameRuleManager.onInventoryClick(event.getCurrentItem().getType(), (Player) event.getWhoClicked());
                 break;
             }
-
             case "§9Hauptmenü §8● ": {
                 event.setCancelled(true);
                 mainInventoryClickEvent(event.getCurrentItem().getType(), (Player) event.getWhoClicked());
@@ -47,47 +46,37 @@ public class InventoryListener implements Listener {
                 gameSettingsInventoryClickEvent(event.getCurrentItem().getType(), (Player) event.getWhoClicked());
                 break;
             }
-
             case "§9Jutils-Einstellungen §8● ": {
                 event.setCancelled(true);
                 jutilsSettingsInventoryClickEvent(event.getCurrentItem().getType(), (Player) event.getWhoClicked());
                 break;
             }
-
             case "§9Timer-Einstellungen §8● ": {
                 event.setCancelled(true);
                 timerInventoryClickEvent(event.getCurrentItem().getType(), (Player) event.getWhoClicked(), event.getClick());
                 break;
             }
-
         }
 
         for (ChallengeManager.Challenges challenge : ChallengeManager.Challenges.values()) {
-
-            if(challenge.getChallenge().getInventoryName() == null) {
+            if (challenge.getChallenge().getInventoryName() == null) {
                 continue;
             }
-
             String title = String.valueOf(event.getView().title());
-
-            if(title.contains(challenge.getChallenge().getInventoryName())) {
+            if (title.contains(challenge.getChallenge().getInventoryName())) {
                 event.setCancelled(true);
-
-                if(event.getCurrentItem().getType() == Material.BLACK_STAINED_GLASS_PANE) {
+                if (event.getCurrentItem().getType() == Material.BLACK_STAINED_GLASS_PANE) {
                     int playerSide = ChallengeInventory.playerSides.get(player.getUniqueId());
                     for (ChallengeInventory.SIDE side : ChallengeInventory.SIDE.values()) {
-                        if(side.getSide() == playerSide) {
+                        if (side.getSide() == playerSide) {
                             player.openInventory(ChallengeInventory.getChallengeInventory(side, player));
                         }
                     }
                     return;
                 }
-
                 challenge.getChallenge().onInventoryClick((Player) event.getWhoClicked(), event.getClick(), event.getCurrentItem().getType(), event.getClickedInventory());
             }
-
         }
-
     }
 
     private static void gameSettingsInventoryClickEvent(Material material, Player player) {
@@ -99,7 +88,6 @@ public class InventoryListener implements Listener {
                 player.openInventory(GameSettingsInventory.getGameSettingsInventory());
                 break;
             }
-
             case WITHER_ROSE: {
                 new ConfigManager(ConfigManager.CONFIGS.CONFIG).toggleBool("wither");
                 player.openInventory(GameSettingsInventory.getGameSettingsInventory());
@@ -125,78 +113,58 @@ public class InventoryListener implements Listener {
 
     private static void challengeInventoryClickEvent(Material material, Player player, ClickType clickType, int slot) {
 
-        if(material == Material.PLAYER_HEAD) {
-
-            if(slot == 44) {
-
+        if (material == Material.PLAYER_HEAD) {
+            if (slot == 44) {
                 int nextSide = ChallengeInventory.playerSides.get(player.getUniqueId()) + 1;
-
                 for (ChallengeInventory.SIDE side : ChallengeInventory.SIDE.values()) {
-                    if(side.getSide() == nextSide) {
+                    if (side.getSide() == nextSide) {
                         player.openInventory(ChallengeInventory.getChallengeInventory(side, player));
                         return;
                     }
                 }
-
             }
-            if(slot == 36) {
+            if (slot == 36) {
                 int previousSide = ChallengeInventory.playerSides.get(player.getUniqueId()) - 1;
-
                 for (ChallengeInventory.SIDE side : ChallengeInventory.SIDE.values()) {
-                    if(side.getSide() == previousSide) {
+                    if (side.getSide() == previousSide) {
                         player.openInventory(ChallengeInventory.getChallengeInventory(side, player));
                         return;
                     }
                 }
             }
-
         }
 
-        for(int i = 0; i < ChallengeManager.Challenges.values().length; i++) {
-
-            if(!clickType.isLeftClick()) continue;
-
+        for (int i = 0; i < ChallengeManager.Challenges.values().length; i++) {
+            if (!clickType.isLeftClick()) continue;
             ChallengeManager.Challenges challenge = ChallengeManager.Challenges.values()[i];
-
-            if(material == Material.BLACK_STAINED_GLASS_PANE) {
+            if (material == Material.BLACK_STAINED_GLASS_PANE) {
                 player.openInventory(MainInventory.getInventory());
             }
-
-            if(material == challenge.getChallenge().getDisplayItem().getType()) {
+            if (material == challenge.getChallenge().getDisplayItem().getType()) {
                 ChallengeManager.toggleChallenge(challenge);
-
-                if(!ChallengeManager.isChallengeEnabled(challenge)) {
+                if (!ChallengeManager.isChallengeEnabled(challenge)) {
                     challenge.getChallenge().onDisable();
                 }
-
                 int playerSide = ChallengeInventory.playerSides.get(player.getUniqueId());
-
                 for (ChallengeInventory.SIDE side : ChallengeInventory.SIDE.values()) {
-                    if(side.getSide() == playerSide) {
+                    if (side.getSide() == playerSide) {
 
                         player.openInventory(ChallengeInventory.getChallengeInventory(side, player));
                     }
                 }
-
-
             }
-
         }
 
-        for(ChallengeManager.Challenges challenges : ChallengeManager.Challenges.values()) {
-            if(challenges.getChallenge().getDisplayItem().getType() == material) {
-
-                if(!clickType.isRightClick()) continue;
-
-                if(challenges.getChallenge().getSettingsInventory() == null) {
+        for (ChallengeManager.Challenges challenges : ChallengeManager.Challenges.values()) {
+            if (challenges.getChallenge().getDisplayItem().getType() == material) {
+                if (!clickType.isRightClick()) continue;
+                if (challenges.getChallenge().getSettingsInventory() == null) {
                     player.sendMessage(Main.getError() + "§cFür diese Challenge gibt es keine weiteren Einstellungen");
                     continue;
                 }
-
                 player.openInventory(challenges.getChallenge().getSettingsInventory());
             }
         }
-
     }
 
     private static void timerInventoryClickEvent(Material material, Player player, ClickType clickType) {
@@ -207,36 +175,31 @@ public class InventoryListener implements Listener {
                 player.openInventory(JutilsSettingsInventory.getJutilsSettingsInventory());
                 break;
             }
-
             case ANVIL: {
                 TimerManager.toggleDirection();
                 player.openInventory(TimerInventory.getTimerInventory());
                 Timer.restartTimer();
                 break;
             }
-
-            case LIME_DYE: case GRAY_DYE: {
+            case LIME_DYE:
+            case GRAY_DYE: {
                 TimerManager.toggleTimerActivity();
                 player.openInventory(TimerInventory.getTimerInventory());
                 Timer.setPause(true);
                 Timer.restartTimer();
                 break;
             }
-
             case RED_CONCRETE: {
                 Timer.resetTimer();
                 break;
             }
-
             case CLOCK: {
                 TimerManager.setCountdownTime(InventoryBuilder.inventoryClickIntManager(TimerManager.getCountdownTime(), clickType, 0, 1000000, 60, 60, 3600, 3600));
                 player.openInventory(TimerInventory.getTimerInventory());
                 Timer.restartTimer();
                 break;
             }
-
         }
-
     }
 
     private static void jutilsSettingsInventoryClickEvent(Material material, Player player) {
@@ -247,47 +210,37 @@ public class InventoryListener implements Listener {
                 player.openInventory(TimerInventory.getTimerInventory());
                 break;
             }
-
             case BLACK_STAINED_GLASS_PANE: {
                 player.openInventory(MainInventory.getInventory());
             }
-
         }
-
     }
 
     private static void mainInventoryClickEvent(Material material, Player player) {
 
-        if(ChallengeInventory.getChallengeInventory(ChallengeInventory.SIDE.SIDE1, player) == null) {
+        if (ChallengeInventory.getChallengeInventory(ChallengeInventory.SIDE.SIDE1, player) == null) {
             player.sendMessage(Component.text(Main.getError() + "§cEs konnten keine Challenges geladen werden."));
             return;
         }
 
         switch (material) {
 
-
             case GRASS_BLOCK: {
                 player.openInventory(ChallengeInventory.getChallengeInventory(ChallengeInventory.SIDE.SIDE1, player));
                 break;
             }
-
             case CLOCK: {
                 player.openInventory(GameRuleInventory.getGameRuleInventory());
                 break;
             }
-
             case REDSTONE: {
                 player.openInventory(JutilsSettingsInventory.getJutilsSettingsInventory());
                 break;
             }
-
             case DRAGON_HEAD: {
                 player.openInventory(GameSettingsInventory.getGameSettingsInventory());
                 break;
             }
-
         }
-
     }
-
 }
